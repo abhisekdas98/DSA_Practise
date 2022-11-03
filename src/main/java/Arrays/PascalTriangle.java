be3fae80b5caa55643ab9f453984509523ca305c
abhisekdas98 <abhisekdas98@gmail.com>
2022-11-03 08:59:57 +0530

    added approach of Pascal Triangle

--- src/main/java/Arrays/PascalTriangle.java
+++ src/main/java/Arrays/PascalTriangle.java
@@ -1,6 +1,14 @@
 package Arrays;
 import java.util.*;
 import java.io.*;
+/*
+Intuition: When you see the image above, you get a pretty good idea of what you are supposed to do here.
+Think about the image as a matrix now where each line is basically a row in the matrix. So, first things
+first, if you are at the edge of the matrix, the value is 1, that’s for sure. Now, what about the inner elements?
+Well, any inner element is obtained by doing the sum of the 2 values present in the row just above it, i.e., if
+the element is at index (i, j), then matrix[i][j] can be obtained by doing matrix[i – 1][j – 1] + matrix[i – 1][j].
+
+ */
 public class PascalTriangle {
     public static  void main(String args[])throws IOException
     {
